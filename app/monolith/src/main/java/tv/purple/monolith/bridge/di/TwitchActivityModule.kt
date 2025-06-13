b574c5039d4dff101d9d888533d739983eb054fd
nopbreak <nopbreak@gmail.com>
2025-06-13 21:50:18 +0300

    Fix crash

--- app/monolith/src/main/java/tv/purple/monolith/bridge/di/TwitchActivityModule.kt
+++ app/monolith/src/main/java/tv/purple/monolith/bridge/di/TwitchActivityModule.kt
@@ -1,6 +1,5 @@
 package tv.purple.monolith.bridge.di
 
-import android.content.Context
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.internal.GeneratedComponent
@@ -11,14 +10,20 @@ import tv.purple.monolith.core.compat.ClassCompat.callPrivateMethod
 import tv.purple.monolith.core.compat.ClassCompat.getPrivateField
 import tv.twitch.android.app.core.Experience
 import tv.twitch.android.core.activities.TwitchHiltActivity
+import tv.twitch.android.core.user.TwitchAccountManager
 import tv.twitch.android.preferences.PreferenceType
 import tv.twitch.android.provider.experiments.helpers.ChatRepliesExperiment
 import tv.twitch.android.provider.experiments.helpers.MessageEffectsExperiment
 import tv.twitch.android.routing.routers.WebViewDialogRouter
 import tv.twitch.android.shared.badges.ChatBadgeProvider
+import tv.twitch.android.shared.chat.commerce.notices.CommerceNoticeViewStateFactory
 import tv.twitch.android.shared.chat.messages.data.ChatMessageV2Parser
+import tv.twitch.android.shared.chat.messages.notices.UserNoticeViewStateFactory
+import tv.twitch.android.shared.chat.messages.span.ChatMessageSpanFactoryV2
+import tv.twitch.android.shared.chat.messages.ui.ChatMessageFactoryV2
 import tv.twitch.android.shared.chat.settings.preferences.ChatSettingsPreferencesFile
 import tv.twitch.android.shared.chat.settings.readablecolors.ReadableColorsCache
+import tv.twitch.android.shared.community.debug.CommunityDebugSharedPreferences
 import tv.twitch.android.shared.emotes.utils.AnimatedEmotesPresenterUtils
 import tv.twitch.android.shared.experiments.helpers.ClipEditorPortalExperimentImpl
 import tv.twitch.android.shared.preferences.chatfilters.ChatFiltersPreferenceFile
@@ -150,4 +155,33 @@ class TwitchActivityModule {
             cre
         )
     }
+
+    @Provides
+    fun provideChatMessageFactoryV2(
+        var1: TwitchAccountManager,
+        var2: AnnotationSpanHelper,
+        var3: ChatMessageSpanFactoryV2,
+        var4: ColorUtil,
+        var5: CommerceNoticeViewStateFactory,
+        var6: UserNoticeViewStateFactory,
+        var7: MessageEffectsExperiment,
+        var8: ChatRepliesExperiment,
+        var9: Experience
+    ): ChatMessageFactoryV2 {
+        return ChatMessageFactoryV2(
+            var1,
+            var2,
+            var3,
+            var4,
+            var5,
+            var6,
+            var7,
+            var8,
+            var9
+        )
+    }
+
+    @Provides
+    fun provideCommunityDebugSharedPreferences(var1: PreferenceType.Debug): CommunityDebugSharedPreferences =
+        CommunityDebugSharedPreferences(var1)
 }
